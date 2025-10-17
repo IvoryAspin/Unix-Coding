534dcba733e11fedce02d9930b9d5fe4ffd59092
IvoryAspin <inwillowtrees@gmail.com>
2025-10-17 11:59:48 -0400

    Create Helperfunctions.c

---
+++ Helperfunctions.c
@@ -0,0 +1,14 @@
+#include <math.h>
+#include <stdio.h>
+
+// this is a file full of image maniuplation helping functions
+
+int CheckRadius(int TotRow, int TotCols, int Radius, int CurRow, int CurCol){
+        //this will return an in or out flag to the caller about weather or not a given pixel is inside a circle starting at the image center
+
+        int InOrOut = 0;        //Change balue to 1 if current pixel is inside the radius
+
+        printf("You called a function\n");
+
+        return InOrOut;
+}
